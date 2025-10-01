b4ce6a46b381213f913fc663e7de0c48308fedc3
Claire Fletcher <claire.fletcher-harris@kaluza.com>
2025-10-01 16:13:45 +0100

    first challenge of october

---
+++ python/freecode-daily-challenges/2025/2025-10-01-decimal.py
@@ -0,0 +1,21 @@
+# Given a string representing a binary number, return its decimal equivalent as a number.
+
+# A binary number uses only the digits 0 and 1 to represent any number. To convert binary to decimal, multiply each digit by a power of 2 and add them together. Start by multiplying the rightmost digit by 2^0, the next digit to the left by 2^1, and so on. Once all digits have been multiplied by a power of 2, add the result together.
+
+# For example, the binary number 101 equals 5 in decimal because:
+
+# 1 * 2^2 + 0 * 2^1 + 1 * 2^0 = 4 + 0 + 1 = 5
+
+def to_decimal(binary):
+
+    # 0 and 1 multiplied by power of 2
+    # e.g. 2^0 -> 2^(len-1), from right to left
+
+    decimal = 0
+    pointer = len(binary)-1
+    for n in binary:
+        power = pow(2, pointer)
+        decimal = decimal + (int(n) * power)
+        pointer = pointer-1
+
+    return decimal
