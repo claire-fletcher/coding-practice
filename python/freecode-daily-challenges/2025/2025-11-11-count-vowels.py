8f36dbf726dc127b965d5dc643b0860e702efc73
Claire Fletcher <claire.fletcher-harris@kaluza.com>
2025-11-11 09:56:45 +0000

    optimisation of code to learn new syntax

--- python/freecode-daily-challenges/2025/2025-11-11-count-vowels.py
+++ python/freecode-daily-challenges/2025/2025-11-11-count-vowels.py
@@ -1,6 +1,6 @@
 import re
 
-def count(s):
+def count_regex(s):
     # lowercase all so they are the same
     s = s.lower()
 
@@ -12,5 +12,16 @@ def count(s):
 
     # consonants take vowel count from total alpha
     consonants_count = letters_count - vowels_count
+
+    return [vowels_count, consonants_count]
+
+vowels = 'aeiou'
+
+def count(s):
+    s = s.lower()
+
+    # Simplified version that uses sum and generator expression as regex creates lists
+    vowels_count = sum(1 for char in s if char in vowels)
+    consonants_count = sum(1 for char in s if char.isalpha() and char not in vowels)
 
     return [vowels_count, consonants_count]
